232704da3e2ff86d6cb8aee6a80671355f686fd7
Lorezz <point.point@gmail.com>
2026-04-08 17:52:41 +0200

    wip intro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://lorezz.me',
-	integrations: [mdx(), sitemap()],
-	vite: {
+    site: 'https://lorezz.me',
+    integrations: [mdx(), sitemap(), react()],
+    vite: {
     plugins: [tailwindcss()],
   }
 });
